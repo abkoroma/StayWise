4bb3bbedce4c86f7ccad6ccba2bede192ddd8883
Abu Bakarr Koroma <abkoroma@Abus-MacBook-Pro.local>
2025-11-23 00:27:18 -0500

    Initial commits

--- app/api/cabins/[cabinid]/route.ts
+++ app/api/cabins/[cabinid]/route.ts
@@ -1,18 +1,18 @@
 import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET( request: NextRequest, context: { params: { cabinid: string } } ) {
 
 
-    const { cabinid } =  await context.params;
+    const { cabinid } = context.params;
     const cabinId = parseInt(cabinid);
 
     try {
         const [cabin, bookedDates] = await Promise.all([
             getCabin(cabinId), getBookedDatesByCabinId(cabinId)
         ]);
-        return Response.json({ cabin, bookedDates  });
+        return NextResponse.json({ cabin, bookedDates  });
     } catch {
-        return Response.json({ message: "Cabin not found" });
+        return NextResponse.json({ message: "Cabin not found" });
     }
 }
