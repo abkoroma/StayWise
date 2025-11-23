d97918b5d159a7009cdf87d36c2120769b21dece
Abu Bakarr Koroma <abkoroma@Abus-MacBook-Pro.local>
2025-11-23 17:47:08 -0500

    Fix type error in GET handler

--- app/api/cabins/[cabinid]/route.ts
+++ app/api/cabins/[cabinid]/route.ts
@@ -2,15 +2,19 @@ import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
 import { CabinInterface } from "@/app/_utils/pages_interface";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET( request: NextRequest, params: { cabinid: string } ):
-    Promise<NextResponse<{ cabin: CabinInterface; bookedDates: Date[] }> | NextResponse<{ message: string }>> {
+export async function GET(
+    request: NextRequest, 
+    context: { params: { cabinid: string } } 
+): Promise<
+    NextResponse<{ cabin: CabinInterface; bookedDates: Date[] }> | 
+    NextResponse<{ message: string }>
+> {
 
-
-    const { cabinid } = await params;
+    const { cabinid } = context.params;
     //const cabinId = parseInt(cabinid);
 
     try {
-        const cabinId = parseInt(cabinid);
+        const cabinId = parseInt(cabinid, 10);
         const [cabin, bookedDates] = await Promise.all([
             getCabin(cabinId), getBookedDatesByCabinId(cabinId)
         ]);
