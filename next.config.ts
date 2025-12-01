16ce5ca9f61c23d5f3f21574e37f95b7303ae337
Abu Bakarr Koroma <abkoroma@Abus-MacBook-Pro.local>
2025-12-01 00:01:38 -0500

    config updates

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     images: {
-      remotePatterns: [new URL('https://ppgfdorfwmgajdiewbnx.supabase.co/storage/v1/object/public/cabin-images/**')],
-      domains: ['lh3.googleusercontent.com'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'ppgfdorfwmgajdiewbnx.supabase.co',
+          pathname: '/storage/v1/object/public/cabin-images/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+          pathname: '/**',
+        }
+        //new URL('https://ppgfdorfwmgajdiewbnx.supabase.co/storage/v1/object/public/cabin-images/**')
+      ],
+      //domains: ['lh3.googleusercontent.com'],
     },
 };
 
